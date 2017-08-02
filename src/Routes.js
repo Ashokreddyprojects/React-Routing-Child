bd2569ee200b2f3de69a5f04fee9b4dc6199332c
Ashok Reddy <ashokcse505@gmail.com>
2017-08-02 20:11:20 +0530

    Child

---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import RegisterPage from './Pages/RegisterPage';
+import LoginPage from './Pages/LoginPage';
+export const history = createHistory();
+//console.log('Router', history);
+const Routes = () => (
+    <Router history={history}>
+        <div>
+            <Route exact path="/" component={RegisterPage} />
+            <Route path="/LoginPage" component={LoginPage} />
+       
+        </div>
+    </Router>
+);
+export default Routes;
+
